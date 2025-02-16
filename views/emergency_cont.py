591a50db643b2814a02291a915d28012fc467df4
Alice L <aliceyyl2003@gmail.com>
2025-02-16 00:14:28 -0500

    Added function to delete contacts

--- views/emergency_cont.py
+++ views/emergency_cont.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 st.title("Emergency Contacts")
 
-from forms.contact import contact_form
+from forms.contact import *
 
 @st.dialog("Enter your emergency contact")
 def show_contact_form():
@@ -16,7 +16,21 @@ if st.button ("Please Enter Emergency Contact(s)👆"):
 st.subheader("Saved Emergency Contacts")
 
 if "contacts" in st.session_state and st.session_state.contacts:
-    st.dataframe(st.session_state.contacts, use_container_width=True)  # Display contacts as a table
+    # Display table headers
+    headers = ["Name", "Relation", "Phone", "Email", "Delete Contact"]
+    cols = st.columns(5)
+    for col, header in zip(cols, headers):
+         col.write(f"**{header}**")
+    # Display contacts
+    for contact in st.session_state.contacts:
+        cols = st.columns(5, border=True)
+        cols[0].write(contact["Name"])
+        cols[1].write(contact["Relation"])
+        cols[2].write(contact["Phone"])
+        cols[3].write(contact["Email"])
+        if cols[4].button("Delete", key=contact["Cont_id"]):
+             delete_contact(contact["Cont_id"])
+             st.rerun()
 else:
     st.info("No emergency contacts added yet.")
 
